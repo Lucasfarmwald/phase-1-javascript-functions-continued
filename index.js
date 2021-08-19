59af59f3d86e409a5d0b35ac584891c944f9c1cb
lucasfarmwald <farmwaldlucas@gmail.com>
2021-08-18 21:10:07 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Your code here
+function saturdayFun(message = "roller-skate") {
+    return (`This Saturday, I want to ${message}!`);
+}
+function mondayWork(quote = "go to the office") {
+    return (`This Monday, I will ${quote}.`);
+}
+function wrapAdjective(){
+    function inner(name = "special"){
+        return (`${name}`)
+    }
+}
